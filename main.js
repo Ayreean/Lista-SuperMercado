a6ab7d94649e6402de0ccd5dc7686cc32b20d908
Carlos Gabriel <carlosgabrielmennabarretocor@gmail.com>
2023-07-18 15:57:14 -0300

    Algumas melhorias

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 let items = [];
+const adicionar = document.querySelector("input[type=submit]")
 
-document.querySelector("input[type=submit]").addEventListener("click", () => {
+if(adicionar) {
+adicionar.addEventListener("click", () => {
     const product = document.getElementById("nome-produto");
     const price = document.getElementById("price");
     items.push({ nome: product.value, valor: price.value });
 
-
-
-    /* modelo a ser seguido
-     <div class="lista-produtos-single">
-            <h3>Cheetos</h3>
-            <h3 class="product-price"><span>R$10,00</span></h3>
-        </div>
-    */
+    if (product.value === "" || price.value === "") {
+        alert("Preencha todos os campos");
+        return;
+    }
 
     let listaProdutos = document.querySelector(".lista-produto");
     let soma = 0;
@@ -20,9 +18,11 @@ document.querySelector("input[type=submit]").addEventListener("click", () => {
     items.map((val) => {
         soma += parseFloat(val.valor);
         listaProdutos.innerHTML += `
+        <div class="lista-produto"> <h2>Lista de Produtos</h2>
         <div class="lista-produtos-single">
             <h3>${val.nome}</h3>
             <h3 class="product-price"><span>R$${val.valor}</span></h3>
+        </div>
         </div>
         `;
     });
@@ -34,3 +34,21 @@ document.querySelector("input[type=submit]").addEventListener("click", () => {
     elementoSoma.innerHTML = `R$${soma.toFixed(2)}`; 
 
 });
+} else {
+    console.log("Ocorreu um erro ao clicar no botão de cadastrar");
+}
+
+let reset = document.querySelector('button[name=limpar]');
+if (reset) {
+  reset.addEventListener("click", () => {
+    items = [];
+    let listaProdutos = document.querySelector(".lista-produto");
+    listaProdutos.innerHTML = "";
+    let elementoSoma = document.querySelector(".soma-total h1");
+    elementoSoma.innerHTML = `Total: R$0,00`;
+  }); 
+} else {
+    console.log("Ocorreu um erro ao clicar no botão de limpar");
+}
+
+
